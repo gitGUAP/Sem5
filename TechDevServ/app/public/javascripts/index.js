dfe0c6dccff8a79a0d20ebe53422e4b264c7021e
Grishy <mail@grishy.ru>
2018-09-18 21:30:35 +0300

    2

--- TechDevServ/app/public/javascripts/index.js
+++ TechDevServ/app/public/javascripts/index.js
@@ -1,16 +1,19 @@
 const $list = $(".g-list");
-const el = `
-<a href="#" class="list-group-item list-group-item-action flex-column align-items-start active">
-    <div class="d-flex w-100 justify-content-between">
-    <h5 class="mb-1">List group item heading</h5>
-    <small>3 days ago</small>
-    </div>
-    <p class="mb-1">Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit.</p>
-    <small>Donec id elit non mi porta.</small>
-</a>`;
+
+function getCard(data) {
+  return `
+<div class="card">
+  <div class="g-index_img" style="background-image:url('${data.img}');"></div>
+  <div class="card-body">
+    <h5 class="card-title">${data.name}</h5>
+    <p class="card-text">${data.desc}</p>
+    <!-- <a href="#" class="btn btn-primary">Go somewhere</a> -->
+  </div>
+</div>`;
+}
 
 $.getJSON("/api/records", function(data) {
-  for (const i of data) {
-    $list.append(el);
+  for (const el of data) {
+    $list.append(getCard(el));
   }
 });
