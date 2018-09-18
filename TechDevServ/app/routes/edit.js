1201b66228649dcf17026846f569de50092f427b
Grishy <mail@grishy.ru>
2018-09-18 22:35:20 +0300

    3

--- TechDevServ/app/routes/edit.js
+++ TechDevServ/app/routes/edit.js
@@ -1,14 +1,50 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
+const db = require('../src/db');
 
-/* GET home page. */
-router.get("/", function(req, res, next) {
-  const login = req.cookies.token === "secret" ? true : false;
+router.get('/', function(req, res, next) {
+  const login = req.cookies.token === 'secret' ? true : false;
   if (!login) {
-    return res.redirect("/");
+    return res.redirect('/');
   }
 
-  res.render("edit", { title: "Edit", login });
+  db.Item.find({}, (err, data) => {
+    if (err) {
+      return next(err);
+    }
+
+    res.render('edit', {title: 'Edit', login, data});
+  });
+});
+
+router.get('/delete/:id', function(req, res, next) {
+  const login = req.cookies.token === 'secret' ? true : false;
+  if (!login) {
+    return res.redirect('/');
+  }
+
+  db.Item.deleteOne({_id: req.params.id}, (err, data) => {
+    if (err) {
+      return next(err);
+    }
+
+    return res.redirect('/edit');
+  });
+});
+
+router.get('/item/:id', function(req, res, next) {
+  const login = req.cookies.token === 'secret' ? true : false;
+  if (!login) {
+    return res.redirect('/');
+  }
+
+  db.Item.findOne({_id: req.params.id}, (err, data) => {
+    if (err) {
+      return next(err);
+    }
+
+    res.render('new', {title: 'New', login, ...data._doc});
+  });
 });
 
 module.exports = router;
--- TechDevServ/app/routes/new.js
+++ TechDevServ/app/routes/new.js
@@ -4,6 +4,38 @@ const uuidv1 = require("uuid/v1");
 const path = require("path");
 const db = require("../src/db");
 
+router.post("/update/:id", function(req, res, next) {
+  const login = req.cookies.token === "secret" ? true : false;
+  if (!login) {
+    return res.redirect("/");
+  }
+
+  db.Item.updateOne({ _id: req.params.id }, { $set: req.body }, (err, data) => {
+    if (err) {
+      return next(err);
+    }
+
+    return res.redirect("/edit");
+  });
+
+  try {
+    const img = req.files.img;
+    const imgExt = path.extname(img.name);
+    const name = uuidv1() + imgExt;
+    const pathToImg = `/data/${name}`;
+
+    img.mv(`./public${pathToImg}`, function(err) {
+      if (err) return res.status(500).send(err);
+
+      db.Item.updateOne(
+        { _id: req.params.id },
+        { $set: { img: pathToImg } },
+        (err, data) => {}
+      );
+    });
+  } catch (error) {}
+});
+
 router.get("/", function(req, res, next) {
   const login = req.cookies.token === "secret" ? true : false;
   if (!login) {
