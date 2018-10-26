98353c9d849fa59cccaccc667b79d054740c2852
Grishy <mail@grishy.ru>
2018-10-26 23:01:26 +0300

    fix

--- TechDevServ/app/routes/REST.js
+++ TechDevServ/app/routes/REST.js
@@ -3,35 +3,26 @@ const router = express.Router();
 const db = require("../src/db");
 
 router.get("/records", function(req, res, next) {
-  db.Item.find({}, (err, data) => {
-    if (err) {
-      return next(err);
-      throw err;
-    }
+    db.Item.find({}, (err, data) => {
+        if (err) {
+            return next(err);
+            throw err;
+        }
 
-    res.send(data);
-  });
+        res.send(data);
+    });
 });
 
 router.post("/login", function(req, res, next) {
-  const userData = { ...req.body };
-
-  db.User.findOne(userData, (err, data) => {
-    if (err) {
-      return next(err);
-    }
-    if (data === null) {
-      return res.redirect("/");
-    }
+    const userData = {...req.body };
 
     res.cookie("token", "secret", { maxAge: 900000 });
     return res.redirect("/");
-  });
 });
 
 router.post("/logout", function(req, res, next) {
-  res.clearCookie("token");
-  return res.redirect("/");
+    res.clearCookie("token");
+    return res.redirect("/");
 });
 
 module.exports = router;
