5bf0f69e1fea92e636f303851da7f7eaa6e04d27
Grishy <mail@grishy.ru>
2018-10-27 00:00:03 +0300

    kafka

--- TechDevServ/app/kafka.js
+++ TechDevServ/app/kafka.js
@@ -2,38 +2,35 @@ const kafka = require("kafka-node");
 const uuid = require("uuid");
 
 const client = new kafka.Client("localhost:2181", "my-client-id", {
-  sessionTimeout: 300,
-  spinDelay: 100,
-  retries: 2
+    sessionTimeout: 300,
+    spinDelay: 100,
+    retries: 2
 });
 
 const producer = new kafka.HighLevelProducer(client);
 producer.on("ready", function() {
-  console.log("Kafka Producer is connected and ready.");
-  KafkaService.sendRecord()
+    console.log("Kafka Producer is connected and ready.");
+    KafkaService.sendRecord()
 });
 
 // For this demo we just log producer errors to the console.
 producer.on("error", function(error) {
-  console.error(error);
+    console.error(error);
 });
 
 const KafkaService = {
     sendRecord: (callback = () => {}) => {
- 
+
         const buffer = new Buffer.from(JSON.stringify("fsdfsdf"));
- 
+
         // Create a new payload
-        const record = [
-            {
-                topic: "webevents.dev",
-                messages: buffer,
-                attributes: 1 /* Use GZip compression for the payload */
-            }
-        ];
- 
+        const record = [{
+            topic: "webevents.dev",
+            messages: buffer,
+            attributes: 1 /* Use GZip compression for the payload */
+        }];
+
         //Send record to Kafka and log result/error
         producer.send(record, callback);
     }
 };
- 
