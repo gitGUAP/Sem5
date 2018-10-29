5cd217f5fc1bd46b1588a199bca04f44fe2e4578
Sergey <mail@grishy.ru>
2018-10-29 22:54:13 +0300

    start

--- Object-oriented programming/main.js
+++ Object-oriented programming/main.js
@@ -1,1 +1,29 @@
-console.log(45678);
+window.onload  = function name(params) {
+    const cells = document.querySelectorAll(".cell")
+    const imgs = [
+        "p-d.png",
+        "p-l.png",
+        "p-r.png",
+    ]
+
+    for (const el of cells) {
+        el.style.backgroundImage = `url('./img/${imgs[Math.floor(Math.random()*imgs.length)]}')`;
+    }
+
+    startWater(30);
+}
+
+function startWater(start) {
+    console.log("Stat water");
+    
+    let t = setInterval(() => {
+        start -= 1;
+
+        document.querySelector(".timer").innerHTML = start + "s";
+
+        if (start < 0 ) {
+            clearInterval(t);
+            console.log("WATER");
+        }
+    },1000)
+}
